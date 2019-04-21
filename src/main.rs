0803be53c3ce9443c432aac1e26ce0780c0f33b2
ncoif <github@ncoif.net>
2019-04-21 18:20:03 +0100

    create_h_tunnel and create_v_tunnel

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate tcod;
 
+use std::cmp;
 use tcod::colors::{self, Color};
 use tcod::console::*;
 
@@ -106,6 +107,18 @@ fn create_room(room: Rect, map: &mut Map) {
     }
 }
 
+fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
+    for x in cmp::min(x1, x2)..=cmp::max(x1, x2) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
+    for y in cmp::min(y1, y2)..=cmp::max(y1, y2) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
 fn make_map() -> Map {
     // fill the map with blocked tiles
     let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
@@ -115,6 +128,7 @@ fn make_map() -> Map {
     let room2 = Rect::new(50, 15, 10, 15);
     create_room(room1, &mut map);
     create_room(room2, &mut map);
+    create_h_tunnel(25, 55, 23, &mut map);
 
     map
 }
