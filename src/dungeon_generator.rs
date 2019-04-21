35e75f05eca18eb7451d73b8d2321d531f5ef17b
ncoif <github@ncoif.net>
2019-04-21 18:40:23 +0100

    Move dungeon generation in a new file

---
+++ src/dungeon_generator.rs
@@ -0,0 +1,88 @@
+use std::cmp;
+
+pub const MAP_WIDTH: i32 = 80;
+pub const MAP_HEIGHT: i32 = 50;
+
+pub type Map = Vec<Vec<Tile>>;
+
+pub fn is_blocked(map: &Map, x: i32, y: i32) -> bool {
+    map[x as usize][y as usize].blocked
+}
+
+/// a tile of the map, and it's properties
+#[derive(Clone, Copy, Debug)]
+pub struct Tile {
+    blocked: bool,
+    block_sight: bool,
+}
+
+impl Tile {
+    pub fn empty() -> Self {
+        Tile {
+            blocked: false,
+            block_sight: false,
+        }
+    }
+
+    pub fn wall() -> Self {
+        Tile {
+            blocked: true,
+            block_sight: true,
+        }
+    }
+}
+
+/// a rectencular room
+#[derive(Clone, Copy, Debug)]
+struct Rect {
+    x1: i32,
+    y1: i32,
+    x2: i32,
+    y2: i32,
+}
+
+impl Rect {
+    pub fn new(x: i32, y: i32, w: i32, h: i32) -> Self {
+        Rect {
+            x1: x,
+            y1: y,
+            x2: x + w,
+            y2: y + h,
+        }
+    }
+}
+
+/// place a rectangle on the map
+fn create_room(room: Rect, map: &mut Map) {
+    for x in (room.x1 + 1)..room.x2 {
+        for y in (room.y1 + 1)..room.y2 {
+            map[x as usize][y as usize] = Tile::empty();
+        }
+    }
+}
+
+fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
+    for x in cmp::min(x1, x2)..=cmp::max(x1, x2) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
+    for y in cmp::min(y1, y2)..=cmp::max(y1, y2) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+pub fn make_map() -> Map {
+    // fill the map with blocked tiles
+    let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+
+    // create two rooms
+    let room1 = Rect::new(20, 15, 10, 15);
+    let room2 = Rect::new(50, 15, 10, 15);
+    create_room(room1, &mut map);
+    create_room(room2, &mut map);
+    create_h_tunnel(25, 55, 23, &mut map);
+
+    map
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 extern crate tcod;
 
-use std::cmp;
+mod dungeon_generator;
+
+use dungeon_generator::*;
 use tcod::colors::{self, Color};
 use tcod::console::*;
 
@@ -9,60 +11,12 @@ const SCREEN_HEIGHT: i32 = 50;
 
 const LIMIT_FPS: i32 = 20;
 
-const MAP_WIDTH: i32 = 80;
-const MAP_HEIGHT: i32 = 50;
-
 const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
 const COLOR_DARK_GROUND: Color = Color {
     r: 50,
     g: 50,
     b: 150,
 };
-
-type Map = Vec<Vec<Tile>>;
-
-/// a tile of the map, and it's properties
-#[derive(Clone, Copy, Debug)]
-struct Tile {
-    blocked: bool,
-    block_sight: bool,
-}
-
-impl Tile {
-    pub fn empty() -> Self {
-        Tile {
-            blocked: false,
-            block_sight: false,
-        }
-    }
-
-    pub fn wall() -> Self {
-        Tile {
-            blocked: true,
-            block_sight: true,
-        }
-    }
-}
-
-/// a rectencular room
-#[derive(Clone, Copy, Debug)]
-struct Rect {
-    x1: i32,
-    y1: i32,
-    x2: i32,
-    y2: i32,
-}
-
-impl Rect {
-    pub fn new(x: i32, y: i32, w: i32, h: i32) -> Self {
-        Rect {
-            x1: x,
-            y1: y,
-            x2: x + w,
-            y2: y + h,
-        }
-    }
-}
 
 /// Generic object that represents an ASCII character on the screen
 /// eg: the player, a monster, an item, etc...
@@ -80,7 +34,7 @@ impl Object {
     }
 
     pub fn move_by(&mut self, dx: i32, dy: i32, map: &Map) {
-        if !map[(self.x + dx) as usize][(self.y + dy) as usize].blocked {
+        if !is_blocked(&map, self.x + dx, self.y + dy) {
             self.x += dx;
             self.y += dy;
         }
@@ -98,41 +52,6 @@ impl Object {
     }
 }
 
-/// place a rectangle on the map
-fn create_room(room: Rect, map: &mut Map) {
-    for x in (room.x1 + 1)..room.x2 {
-        for y in (room.y1 + 1)..room.y2 {
-            map[x as usize][y as usize] = Tile::empty();
-        }
-    }
-}
-
-fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
-    for x in cmp::min(x1, x2)..=cmp::max(x1, x2) {
-        map[x as usize][y as usize] = Tile::empty();
-    }
-}
-
-fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
-    for y in cmp::min(y1, y2)..=cmp::max(y1, y2) {
-        map[x as usize][y as usize] = Tile::empty();
-    }
-}
-
-fn make_map() -> Map {
-    // fill the map with blocked tiles
-    let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
-
-    // create two rooms
-    let room1 = Rect::new(20, 15, 10, 15);
-    let room2 = Rect::new(50, 15, 10, 15);
-    create_room(room1, &mut map);
-    create_room(room2, &mut map);
-    create_h_tunnel(25, 55, 23, &mut map);
-
-    map
-}
-
 fn render_all(root: &mut Root, con: &mut Offscreen, objects: &[Object], map: &Map) {
     // draw all objects
     for object in objects {
@@ -142,7 +61,7 @@ fn render_all(root: &mut Root, con: &mut Offscreen, objects: &[Object], map: &Ma
     // draw all the tiles of the map
     for y in 0..MAP_HEIGHT {
         for x in 0..MAP_WIDTH {
-            let wall = map[x as usize][y as usize].blocked;
+            let wall = is_blocked(&map, x, y);
             if wall {
                 con.set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
             } else {
@@ -201,7 +120,7 @@ fn main() {
     let mut objects = [player];
 
     // generate the map
-    let map = make_map();
+    let map = dungeon_generator::make_map();
 
     // main game loop
     while !root.window_closed() {
